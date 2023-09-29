47043dcd21159f708f490a3e856264e8c3c0c6f2
WhatEverKid07 <eugeniosanc22@outlook.com>
2023-09-29 12:44:55 +0100

    uhhhhh

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -10,9 +10,12 @@ public class PlayerController : MonoBehaviour
     Rigidbody2D playerObject;
     public GameObject groundChecker;
     public LayerMask whatisground;
-    public float jumpSpeed = 10.0f;
-    float maxSpeed = 5.0f;
-    bool isOnGround = false;
+    public float jumpSpeed = 8.0f;
+    bool jumping = false;
+    public float maxSpeed = 5.0f;
+    bool isOnGround = true;
+    float jumpTime;
+    public float buttonTime = 0.3f;
 
    
 
@@ -23,7 +26,7 @@ public class PlayerController : MonoBehaviour
 
        //Fined the Rigibody2D component that is attached to the same object as this script
        playerObject = GetComponent<Rigidbody2D>();
-
+      
        
 
     }
@@ -37,17 +40,31 @@ public class PlayerController : MonoBehaviour
 
 
         //Change the X velocity of the Rigidboady2D to be equal to the movement value
-        playerObject.velocity = new Vector2(movementValueX * 20, playerObject.velocity.y);
+        playerObject.velocity = new Vector2(movementValueX * 10, playerObject.velocity.y);
 
 
         //Check to see if the ground object is touching the ground
+        
         isOnGround = Physics2D.OverlapCircle(groundChecker.transform.position, 1.0f, whatisground);
 
         if (Input.GetButtonDown("Jump"))
         {
             playerObject.velocity = new Vector2(playerObject.velocity.x, jumpSpeed);
+
+            jumping = true;
+            jumpTime = 0;
         }
-        
+
+        if(jumping)
+        {
+            jumpTime += Time.deltaTime;
+        }
+
+        if (Input.GetButtonDown("Space") | jumpTime > buttonTime)
+        {
+            jumping = false;
+        }
+
     }
 
 
